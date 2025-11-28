05706d9856fc2f3f52d2109bcb7afe195cea149a
walid oumechtak <woumechtak@gmail.com>
2025-11-28 11:50:12 +0100

    remove garbage files

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs"; 
-
-export default function Page() {
-    return (
-            <div className="flex justify-center items-center min-h-screen bg-gray-100">
-                <SignIn />;
-            </div>
-        )
-}
